f7e595e95c8c44e1a0b6b44c89c593874a539601
Junior Streichan <juniorstreichan.dev@gmail.com>
2019-05-02 08:58:15 -0400

    [feat] Add clusterization

--- src/server.js
+++ src/server.js
@@ -1,18 +1,20 @@
+import 'regenerator-runtime/runtime';
 import mongoose from 'mongoose';
+import cluster from 'cluster';
+import os from 'os';
 import App from './app';
 import dotenvConfig from './configs/dotenv-config';
 
-dotenvConfig();
+function init() {
+  dotenvConfig();
+  mongoose.connect(process.env.DATABASE_CONNECTION, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+  });
 
-mongoose.connect(process.env.DATABASE_CONNECTION, {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-});
-
-const port = process.env.PORT || 3334;
-App.listen(port);
-
-console.log(`---------------------------------------------------------------------------------------------------------------------
+  const port = process.env.PORT || 3334;
+  App.listen(port);
+  console.log(`---------------------------------------------------------------------------------------------------------------------
 ########  #### ########  ##       ####    ###       ##     ## ########  ######  ########       ###    ########  ####    
 ##     ##  ##  ##     ## ##        ##    ## ##      ##     ## ##       ##    ##    ##         ## ##   ##     ##  ##     
 ##     ##  ##  ##     ## ##        ##   ##   ##     ##     ## ##       ##          ##        ##   ##  ##     ##  ##     
@@ -20,6 +22,18 @@ console.log(`-------------------------------------------------------------------
 ##     ##  ##  ##     ## ##        ##  #########     ##   ##  ##             ##    ##       ######### ##         ##     
 ##     ##  ##  ##     ## ##        ##  ##     ##      ## ##   ##       ##    ##    ##       ##     ## ##         ##     
 ########  #### ########  ######## #### ##     ##       ###    ########  ######     ##       ##     ## ##        #### 
----------------------------------------------------------------------------------------------------------------------`);
+---------------------------------------------------------------------------------------------------------------------
+🚀🚀🚀     SERVER RODANDO NA PORTA ${port} PROCCESS ${process.pid}      🚀🚀🚀`);
+}
 
-console.log(`🚀🚀🚀     SERVER RODANDO NA PORTA ${port}      🚀🚀🚀`);
+if (process.env.NODE_ENV !== 'dev') {
+  if (!cluster.isMaster) {
+    init();
+  } else {
+    for (let i = 0; i < os.cpus().length; i += 1) {
+      cluster.fork();
+    }
+  }
+} else {
+  init();
+}
